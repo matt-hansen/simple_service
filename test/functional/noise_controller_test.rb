bb17658f056898f42fbee946be9d5d83bdddeaa4
Matt Hansen <matt.hansen@oregonstate.edu>
2012-07-14 11:03:45 -0700

    add in some tests

--- test/functional/noise_controller_test.rb
+++ test/functional/noise_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class NoiseControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should say OK when pinged" do
+    get :index, { 'q' => 'Ping' }
+    assert_response :success
+    assert_equal assigns(:text_to_render), "OK"
+  end
+
+  test "should correctly handle Expression 5 + 5 - 7 - 7" do
+    get :index, { 'q' => 'Expression', 'd' => 'What is 5 + 5 - 7 - 7?' }
+    assert_response :success
+    assert_equal assigns(:text_to_render), (5+5-7-7)
+  end
+
+  test "should correctly handle 18 + 12 - 4 - 6 * 2" do
+    get :index, { 'q' => 'Expression', 'd' => 'What is 18 + 12 - 4 - 6 * 2?' }
+    assert_response :success
+    assert_equal assigns(:text_to_render), (18+12-4-6*2)
+  end
+
+  test "should provide a github url when asked for the source" do
+    get :index, { 'q' => 'Source' }
+    assert_response :success
+    assert_equal assigns(:text_to_render), "https://github.com/matt-hansen/simple_service"
+  end
+  
+  test "should provide a default response when no params are passed" do
+    get :index
+    assert_response :success
+    assert_equal assigns(:text_to_render), "Default Response"
+  end  
 end
