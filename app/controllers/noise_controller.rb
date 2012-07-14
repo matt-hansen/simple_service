7bf5fbe88edcd1f1bfad22d0f7bf2a4c917a75eb
Matt Hansen <matt.hansen@oregonstate.edu>
2012-07-14 11:03:13 -0700

    refactor controller

--- app/controllers/noise_controller.rb
+++ app/controllers/noise_controller.rb
@@ -1,7 +1,21 @@
 class NoiseController < ApplicationController
-  
+
   def index
-    render :text => "OK"
+    case params[:q]
+    when "Ping"
+      @text_to_render = "OK"
+    when "Expression"
+      expression = params[:d]
+      # Grab the expression part of the string by taking a range from the index of the
+      # first number to the index of the last number
+      expression = expression[expression.index(/\d/)..expression.rindex(/\d/)]
+      @text_to_render = eval(expression)
+    when "Source"
+      @text_to_render = "https://github.com/matt-hansen/simple_service"
+    end
+
+    @text_to_render ||= "Default Response"
+    render :text => @text_to_render
   end
-  
+
 end
