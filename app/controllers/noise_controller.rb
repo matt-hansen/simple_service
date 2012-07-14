bb151b0574256027c1dee63656f4e11852b21aa2
Matt Hansen <matt.hansen@oregonstate.edu>
2012-07-14 11:19:45 -0700

    add several more question handlers

--- app/controllers/noise_controller.rb
+++ app/controllers/noise_controller.rb
@@ -12,6 +12,19 @@ class NoiseController < ApplicationController
       @text_to_render = eval(expression)
     when "Source"
       @text_to_render = "https://github.com/matt-hansen/simple_service"
+    when "Email Address"
+      @text_to_render = "matt.j.hansen@gmail.com"
+    when "Phone"
+      @text_to_render = "541-224-3842"
+    when "Full Name"
+      @text_to_render = "Matthew J Hansen"
+    when "Years"
+      @text_to_render = 9
+    when "Degree"
+      @text_to_render = "Bachelors of Business Administration with Emphasis " .
+                        "in Computer Information Systems from Idaho State University"
+    when "Position"
+      @text_to_render = "Sr. Web Applications Developer"
     end
 
     @text_to_render ||= "Default Response"
